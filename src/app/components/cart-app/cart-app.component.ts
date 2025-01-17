89208d6186eb93b65c280d455afd4cc1eb5db929
cristiandaza07 <cristiandaza51@gmail.com>
2025-01-17 00:59:33 -0500

    Creación del catalogo con los productos y la función de agregar productos al carrito

--- src/app/components/cart-app/cart-app.component.ts
+++ src/app/components/cart-app/cart-app.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product';
+import { CatalogComponent } from '../catalog/catalog.component';
+import { CartComponent } from '../cart/cart.component';
+import { CartItem } from '../../models/cartItem';
 
 @Component({
-  selector: 'app-cart-app',
-  imports: [],
-  templateUrl: './cart-app.component.html',
-  styleUrl: './cart-app.component.css'
+  selector: 'cart-app',
+  imports: [CatalogComponent, CartComponent],
+  templateUrl: './cart-app.component.html'
 })
-export class CartAppComponent {
+export class CartAppComponent implements OnInit{
 
+  products: Product[] = [];
+
+  items: CartItem[] = [];
+
+  constructor(private service: ProductService){
+
+  }
+
+  ngOnInit(): void {
+    this.products = this.service.findAll();
+  }
+
+  onAddCart(product: Product){
+    const hasItem = this.items.find(item => {
+      return item.product.id === product.id;
+    });
+
+    if(hasItem){
+      this.items = this.items.map(item =>{
+        if(item.product.id === product.id){
+          return {... item, quantity: item.quantity + 1};
+        }
+        return item;
+      });
+    }else{
+      this.items = [... this.items, {product: {... product}, quantity: 1}];
+    }
+  }
 }
---
+++ src/app/data/product.data.ts
@@ -0,0 +1,40 @@
+import { Product } from "../models/product";
+
+export const products: Product[] = [
+    {
+        id: 1,
+        name: 'Bicicleta',
+        description: 'Bicicleta todo terreno profesional',
+        price: 600
+    },
+    {
+        id: 2,
+        name: 'Celular iPhone 13',
+        description: 'Celular de la marca Appel del año 2019',
+        price: 930
+    },
+    {
+        id: 3,
+        name: 'Teclado Asus',
+        description: 'Teclado gamer enfocado en videojuegos y con luces RGB',
+        price: 200
+    },
+    {
+        id: 4,
+        name: 'Portatil Hp',
+        description: 'Computador portatil HP ProBook enfocado para tareas basicas',
+        price: 800
+    },
+    {
+        id: 5,
+        name: 'Bicicleta',
+        description: 'Bicicleta todo terreno profesional',
+        price: 100
+    },
+    {
+        id: 6,
+        name: 'Lampara',
+        description: 'Lampara para escritorio perfecta para hacer diversas tareas con una luz tenue',
+        price: 50
+    }
+];
